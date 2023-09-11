1e8c1875b0da8cfb816cf37eba923b71ea3e6ecd
Mico Kontic <micokontic@gmail.com>
2023-09-11 22:56:02 +0200

    pretty format of all components

--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -1,66 +1,90 @@
-import {useEffect,useState} from 'react'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import AvatarImage from './AvatarImage';
+import { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import AvatarImage from "./AvatarImage";
 
-
-
-export default function Location({name,type,dimension, url,residents}) {
-
-
-  return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent pl={2}>
-        <Typography align="left" variant="h5" color="text.secondary" gutterBottom>
-          PLANET NAME: <Typography variant="h5" component="div"  style={{display: 'inline-block', color:"black"}}>
-          {name}
-        </Typography>
-        </Typography>
-          <Typography variant="h5" color="text.secondary" align="left" gutterBottom>
-          DIMENSION: <Typography variant="h5" component="div"  style={{display: 'inline-block', color:"black"}}>
-          {dimension}
-        </Typography>
-        </Typography>
-         <Typography variant="h5" color="text.secondary"  align="left" gutterBottom>
-          TYPE: <Typography variant="h5" component="div"  style={{display: 'inline-block', color:"black"}}>
-          {type}
-        </Typography>
-        </Typography>
-         <Typography variant="h5" color="text.secondary"  align="left" gutterBottom>
-          RESIDENTS: 
-        </Typography>
-        {
-                residents.length==0?<Typography variant="h5" color="black"  align="left" gutterBottom>
-          NO RESIDENTS
-        </Typography>:
-        <Stack direction="row" spacing={2}>
-          {
-            residents.map((resident,i)=>{
-                if(i<5){
-                    return(
-                        <AvatarImage key={i} resident={resident}>
-
-                        </AvatarImage>
-                    )
-                }
-            })
-          }
-            
-            
-        </Stack>
-}
-        
-        
-      
-        
-      </CardContent>
-      <CardActions style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <Button align="right" size="small"><a href={url} rel="noreferrer" target="_blank" >GO TO PLANET PAGE</a></Button>
-      </CardActions>
-    </Card>
-  );
+export default function Location({ name, type, dimension, url, residents }) {
+	return (
+		<Card sx={{ minWidth: 275 }}>
+			<CardContent pl={2}>
+				<Typography
+					align="left"
+					variant="h5"
+					color="text.secondary"
+					gutterBottom
+				>
+					PLANET NAME:{" "}
+					<Typography
+						variant="h5"
+						component="div"
+						style={{ display: "inline-block", color: "black" }}
+					>
+						{name}
+					</Typography>
+				</Typography>
+				<Typography
+					variant="h5"
+					color="text.secondary"
+					align="left"
+					gutterBottom
+				>
+					DIMENSION:{" "}
+					<Typography
+						variant="h5"
+						component="div"
+						style={{ display: "inline-block", color: "black" }}
+					>
+						{dimension}
+					</Typography>
+				</Typography>
+				<Typography
+					variant="h5"
+					color="text.secondary"
+					align="left"
+					gutterBottom
+				>
+					TYPE:{" "}
+					<Typography
+						variant="h5"
+						component="div"
+						style={{ display: "inline-block", color: "black" }}
+					>
+						{type}
+					</Typography>
+				</Typography>
+				<Typography
+					variant="h5"
+					color="text.secondary"
+					align="left"
+					gutterBottom
+				>
+					RESIDENTS:
+				</Typography>
+				{residents.length == 0 ? (
+					<Typography variant="h5" color="black" align="left" gutterBottom>
+						NO RESIDENTS
+					</Typography>
+				) : (
+					<Stack direction="row" spacing={2}>
+						{residents.map((resident, i) => {
+							if (i < 5) {
+								return <AvatarImage key={i} resident={resident}></AvatarImage>;
+							}
+						})}
+					</Stack>
+				)}
+			</CardContent>
+			<CardActions style={{ display: "flex", justifyContent: "flex-end" }}>
+				<Button align="right" size="small">
+					<a href={url} rel="noreferrer" target="_blank">
+						GO TO PLANET PAGE
+					</a>
+				</Button>
+			</CardActions>
+		</Card>
+	);
 }
--- src/Components/Locations.jsx
+++ src/Components/Locations.jsx
@@ -1,71 +1,64 @@
+import { useEffect, useState } from "react";
+import Location from "./Location";
 
-import {useEffect,useState} from 'react'
-import Location from './Location';
-
-import { experimentalStyled as styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
+	...theme.typography.body2,
+
+	textAlign: "center",
+	color: theme.palette.text.secondary,
 }));
 
-
-
 function Locations() {
+	const [locations, setLocations] = useState([]);
 
-    const [locations,setLocations]=useState([]);
+	const getLocations = () => {
+		fetch("https://rickandmortyapi.com/api/location?page=2%22")
+			.then((response) => {
+				// Check if the response status is OK (status code 200)
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
 
-    const getLocations=()=>{
-        fetch('https://rickandmortyapi.com/api/location?page=2%22')
-  .then(response => {
-    // Check if the response status is OK (status code 200)
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    
-    // Parse the response data as JSON
-    return response.json();
-  })
-  .then(data => {
-    // Do something with the retrieved data
-   setLocations(data.results)
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch
-    console.error('Fetch error:', error);
-  });
-    }
-    
+				// Parse the response data as JSON
+				return response.json();
+			})
+			.then((data) => {
+				// Do something with the retrieved data
+				setLocations(data.results);
+			})
+			.catch((error) => {
+				// Handle any errors that occurred during the fetch
+				console.error("Fetch error:", error);
+			});
+	};
 
-    useEffect(()=>{
-        getLocations()
-    },[])
+	useEffect(() => {
+		getLocations();
+	}, []);
 
-  return (
-     
-      <Grid container spacing={2}>
-        
-          
-            {locations.map(({name,type,dimension,index,url,residents,i})=>{
-
-        return ( <Grid xs={4} key={i}>
-                <Item key={index}>
-                            <Location name={name} type={type} dimension={dimension}  url={url} residents={residents}></Location>
-                </Item> 
-                </Grid>)
-            })}
-        </Grid>
-    
-         
-        
-     
-   
-    
-  )
+	return (
+		<Grid container spacing={2}>
+			{locations.map(({ name, type, dimension, index, url, residents, i }) => {
+				return (
+					<Grid xs={4} key={i}>
+						<Item key={index}>
+							<Location
+								name={name}
+								type={type}
+								dimension={dimension}
+								url={url}
+								residents={residents}
+							></Location>
+						</Item>
+					</Grid>
+				);
+			})}
+		</Grid>
+	);
 }
 
-export default Locations
+export default Locations;
