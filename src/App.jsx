cdcb712ddb80b91d02424b97653676218db3ac3d
Mico Kontic <micokontic@gmail.com>
2023-09-11 22:52:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Locations from './Components/Locations'
+import Typography from '@mui/material/Typography';
+
+
+function App() {
+
+
+  return (
+    <>
+    
+    <Typography variant="h2" component="div" m={3}  style={{display: 'inline-block', color:"black"}}>
+          Planets in Rick and Morty
+        </Typography>
+     <Locations></Locations>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/AvatarImage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect,useState } from 'react'
+import Avatar from '@mui/material/Avatar';
+
+
+function AvatarImage({resident}) {
+
+    const[imageUrl,setImageUrl]=useState(null)
+
+    useEffect(()=>{
+
+        if(resident){
+
+        fetch(resident)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the response as JSON
+  })
+  .then(data => {
+    // Work with the data here
+    setImageUrl(data.image)
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+
+    }}
+
+    
+    ,[])
+
+
+  return (
+    
+  imageUrl==null?'':<a href={resident} rel="noreferrer" target="_blank" ><Avatar alt="avatar image" src={imageUrl}    sx={{ width: 50, height: 50 }}  /></a>
+     
+     
+     )
+        
+  
+}
+
+export default AvatarImage
